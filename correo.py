f1a6ed0372fbc248d1614414eed419ce575c4197
Daniel020899 <daniel.larosa1@alu.uclm.es>
2020-10-15 19:08:14 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -4,3 +4,31 @@
 def my_email():
     return 'daniel.larosa1@alu.uclm.es'
 
+
+import unittest
+class Test(unittest.TestCase):
+
+    def test_01_devuelve_una_cadena(self):
+        from correo import  my_email
+        self.assertEqual(str,type(my_email()))
+
+    def test_02_contiene_una_arroba(self):
+        from correo import  my_email
+        self.assertEqual(1,my_email().count('@'))
+
+    def test_03_es_direccion_de_alumno(self):
+        from correo import  my_email
+        self.assertTrue(my_email().endswith('@alu.uclm.es'))
+
+    def test_04_no_contiene_espacios(self):
+        from correo import  my_email
+        email = my_email()
+        self.assertFalse(' ' in email)
+        self.assertFalse('\t' in email)
+        self.assertFalse('\n' in email)
+        self.assertFalse('\r' in email)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    unittest.main()
+
